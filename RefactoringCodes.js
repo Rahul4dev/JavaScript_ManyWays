e2602da7533c5a90622ad7adf072b598f562152d
Rahul Singh <114183358+Rahul4dev@users.noreply.github.com>
2022-12-21 19:41:14 +0530

    Create RefactoringCodes.js

---
+++ RefactoringCodes.js
@@ -0,0 +1,4 @@
+// Many Ways' main aim is to make code leaner, understandable and have lower complexity. refactoring of code deals with all of these things and consistently make positive impact on coders life.
+
+// How refactoring help in Coding Interview..
+// For Ex: You got a problem in Your interview that give the counts of the characters come in "Hi there! How are you?". How do you approach to the problem??
